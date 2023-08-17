796d99e27f1998c3b8cdc110fa1d5d9a5b3b8b6b
Denzel <jdenzelmab@gmail.com>
2023-08-17 17:26:12 -0400

    Completed translating from orms to sqlalchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,26 @@
 from models import Dog
 
-def create_table(base):
-    pass
-
-def save(session, dog):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+    
+def save(session, name):
+    session.add(name)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+
+    return dog
